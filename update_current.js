0c824021b70e9d7a5299e4f04d635198f056fe00
Andrey Filippov <afi@i-loto.ru>
2014-05-16 15:38:01 +0400

    в migration_info добавлено новое поле comment, к-рое содержит имя файла миграции

--- update_current.js
+++ update_current.js
@@ -1,7 +1,8 @@
 var migration_info = db.migration_info.findOne();
 if (migration_info) {
   migration_info.version = current;
+  migration_info.comment = comment;
 } else {
-  migration_info = {_id: new ObjectId(), version: current};
+  migration_info = {_id: new ObjectId(), version: current, comment: comment};
 }
 db.migration_info.save(migration_info);
